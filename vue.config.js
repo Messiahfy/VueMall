ecaca5d75550b71e4009c7fc35a162b5b89a45b5
huangfeiyang <huangfeiyang@cvte.com>
2020-11-18 21:18:34 +0800

    添加vue.config.js用于配置路径别名

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+//vue.config.js 是一个可选的配置文件，如果项目的 (和 package.json 同级的) 根目录中存在这个文件，那么它会被 @vue/cli-service 自动加载
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            //路径别名
+            alias: {
+                'assets': '@/assets',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views'
+            }
+        }
+    }
+}
