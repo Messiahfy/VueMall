2ab0349a8d72b8d392f2ec09b72a3a13c3471be0
huangfeiyang <huangfeiyang@cvte.com>
2020-11-19 20:03:10 +0800

    完成首页轮播图和推荐

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,17 +3,24 @@
         <nav-bar class="home-nav">
             <div slot="center">购物街</div>
         </nav-bar>
+        <home-swiper :banners="banners"></home-swiper>
+        <home-recommend-view :recommends="recommends"></home-recommend-view>
     </div>
 </template>
 
 <script>
     import NavBar from "components/common/navbar/NavBar";
     import {getHomeMultiData} from "../../network/home";
+    import HomeSwiper from "./childComps/HomeSwiper";
+    import HomeRecommendView from "./childComps/HomeRecommendView";
+
 
     export default {
         name: "Home",
         components: {
-            NavBar
+            HomeSwiper,
+            NavBar,
+            HomeRecommendView
         },
         data() {
             return {
@@ -24,6 +31,7 @@
         created() {
             getHomeMultiData().then(res => {
                 this.banners = res.data.banner.list;
+                console.log(this.banners)
                 this.recommends = res.data.recommend.list;
             })
         }
